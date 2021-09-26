414b8dfc6412e3846c6fb7bd347cbffc314f0d03
Muhammad Mujir <muhammadmujir011@gmail.com>
2021-09-26 19:47:42 +0700

    create simple work manager

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -0,0 +1,39 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
+import com.example.background.R
+
+class BlurWorker(context: Context, params: WorkerParameters): Worker(context, params) {
+
+    override fun doWork(): Result {
+        val appContext = applicationContext
+
+        makeStatusNotification("Blurring image", appContext)
+
+        return try {
+            val picture = BitmapFactory.decodeResource(
+                appContext.resources,
+                R.drawable.android_cupcake)
+
+            val output = blurBitmap(picture, appContext)
+
+            // Write bitmap to a temp file
+            val outputUri = writeBitmapToFile(appContext, output)
+
+            makeStatusNotification("Output is $outputUri", appContext)
+
+            Result.success()
+        } catch (throwable: Throwable) {
+            Log.e("Error Bluring", "Error applying blur")
+            Result.failure()
+        }
+    }
+}
